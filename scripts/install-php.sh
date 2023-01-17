ee9c08085b93c8ca0514eee26ad1f2848024a625
Pezhvak <pezhvakimv@gmail.com>
2023-01-17 13:24:23 +0330

    Fixed PHP intl extension installation

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -4,18 +4,19 @@
 docker-php-ext-configure opcache --enable-opcache &&
   docker-php-ext-configure gd --enable-gd --with-freetype --with-jpeg --with-webp &&
   docker-php-ext-configure zip &&
+  docker-php-ext-configure intl &&
   docker-php-ext-install -j "$(nproc)" \
     mysqli \
     pdo_mysql \
     sockets \
-    intl \
     bz2 \
     pcntl \
     bcmath \
     exif
 
-# note: for some reason if we build gd with the rest of the extensions it will trow an error in php -v
+# note: for some reason if we build gd,intl with the rest of the extensions it will trow an error in php -v
 docker-php-ext-install -j "$(nproc)" gd
+docker-php-ext-install -j "$(nproc)" intl
 
 # Enable production environment
 mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
