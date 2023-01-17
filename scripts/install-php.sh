8fd24b1d59446badfd389efbe2e22e98f14c7133
Pezhvak <pezhvakimv@gmail.com>
2023-01-17 17:12:54 +0330

    Added PHP intl extension.

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -4,7 +4,6 @@
 docker-php-ext-configure opcache --enable-opcache &&
   docker-php-ext-configure gd --enable-gd --with-freetype --with-jpeg --with-webp &&
   docker-php-ext-configure zip &&
-  docker-php-ext-configure intl &&
   docker-php-ext-install -j "$(nproc)" \
     mysqli \
     pdo_mysql \
@@ -17,6 +16,7 @@ docker-php-ext-configure opcache --enable-opcache &&
 # note: for some reason if we build gd,intl with the rest of the extensions it will trow an error in php -v
 docker-php-ext-install -j "$(nproc)" gd
 install-php-extensions redis
+install-php-extensions intl
 
 # Enable production environment
 mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
