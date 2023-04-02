885eeb0f58952b6cd837094eee9d3729aecb812c
Pezhvak <pezhvakimv@gmail.com>
2023-04-02 17:00:21 +0330

    Disabled cleanup for pcntl installation.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ash
 
 # Remove Build Dependencies
-apk del -f .build-deps
+#apk del -f .build-deps
 # we need these for adding extra extensions in CI
 # rm /usr/local/bin/install-php-extensions
 # rm /usr/local/bin/docker-php-*
